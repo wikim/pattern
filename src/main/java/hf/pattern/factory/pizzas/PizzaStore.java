5c9f3e63238be00a3f7089c096f2572ea66ffebc
nexus <nexus@innotree.com>
2015-03-18 09:41:54 +0900

    test

--- src/main/java/hf/pattern/factory/pizzas/PizzaStore.java
+++ src/main/java/hf/pattern/factory/pizzas/PizzaStore.java
@@ -2,15 +2,25 @@ package hf.pattern.factory.pizzas;
 
 public class PizzaStore {
     SimplePizzaFactory factory;
-
+    NyPizzaFactory nyPizzaFactory;
+    
     public PizzaStore(SimplePizzaFactory factory) {
         this.factory = factory;
     }
+    
+    public PizzaStore(NyPizzaFactory nyPizzaFactory) {
+        this.nyPizzaFactory = nyPizzaFactory;
+    }
+   
 
     public Pizza orderPizza(String type) {
         Pizza pizza;
 
-        pizza = factory.createPizza(type);
+        //pizza = factory.createPizza(type);
+        pizza = nyPizzaFactory.createPizza(type);
+        
+        
+        
 
         pizza.prepare();
         pizza.bake();
--- src/main/java/hf/pattern/factory/pizzas/PizzaTestDrive.java
+++ src/main/java/hf/pattern/factory/pizzas/PizzaTestDrive.java
@@ -13,5 +13,10 @@ public class PizzaTestDrive {
         pizza = store.orderPizza("veggie");
         System.out.println("We ordered a " + pizza.getName() + "\n");
         System.out.println(pizza);
+        
+        NyPizzaFactory nyPizzaFactory = new NyPizzaFactory();
+        PizzaStore nYStore = new PizzaStore(nyPizzaFactory);        
+        pizza = nYStore.orderPizza("veggi");
+        
     }
 }
